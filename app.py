4816a58825eef1894283a66c82a3f81e6cf1d21e
v-a-s-a-v-i <100505394+v-a-s-a-v-i@users.noreply.github.com>
2022-02-27 15:02:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model = joblib.load('iris')
+st.slider('IRIS CLASSIFIER')
+ip = st.text_input('enter the message')
+op = model.predict([ip])
+if st.button('Predict'):
+  st.slider(op[0])
